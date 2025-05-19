4a63709ee173bbe7fc3e26c992d9a1ace8b1a7a2
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-05-20 01:40:56 +0300

    chore: seed sample data

---
+++ Arsenal2025API/Helpers/AuthHelpers.cs
@@ -0,0 +1,17 @@
+namespace Arsenal2025API.Helpers;
+
+public static class AuthHelpers
+{
+    public static readonly Guid SupervisorId = Guid.Parse("0196e9bc-e26f-7cdb-830f-dee67048db5e");
+    public static readonly Guid FanId = Guid.Parse("01234567-89ab-cdef-0123-456789abcdef");
+
+    public static bool VerifyPassword(string password, string passwordHash)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+    }
+    
+    public static string HashPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+}
