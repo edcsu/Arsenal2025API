bf09b9531e52c862e421be17254faf1a840592de
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-05-20 03:03:04 +0300

    feat: add endpoint to get an admin

--- Arsenal2025API/Services/AuthService.cs
+++ Arsenal2025API/Services/AuthService.cs
@@ -1,4 +1,6 @@
 using Arsenal2025API.Data;
+using Arsenal2025API.Dtos;
+using Arsenal2025API.Mappers;
 using Arsenal2025API.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,12 +18,40 @@ public class AuthService : IAuthService
         _logger = logger;
     }
 
-    public async Task<User?> FindUserByUsername(string username, 
+    public async Task<User?> FindUserByUsernameAsync(string username, 
         CancellationToken cancellationToken = default)
     {
-        var admin = await _context.Users
+        var user = await _context.Users
             .FirstOrDefaultAsync(a => a.Username == username, cancellationToken);
         
-        return admin;
+        return user;
+    }
+
+    public async Task<User?> FindUserByEmailAsync(string email, 
+        CancellationToken cancellationToken = default)
+    {
+        var user = await _context.Users
+            .FirstOrDefaultAsync(a => a.Email == email, cancellationToken);
+        
+        return user;
+    }
+
+    public async Task<User> CreateUserAsync(CreateUserRequest request, 
+        CancellationToken cancellationToken = default)
+    {
+        var user = request.ToUser();
+        user.Role = SystemRole.Admin;
+        
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync(cancellationToken);
+        return user;
+    }
+
+    public async Task<User?> FindUserByIdAsync(Guid id, 
+        CancellationToken cancellationToken = default)
+    {
+        var user = await _context.Users.FindAsync([id], 
+            cancellationToken: cancellationToken);
+        return user;
     }
 }
